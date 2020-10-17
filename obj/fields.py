1e33aea8ce93265d41ef0403da2f401741a7d239
liushuochen <2524453728@qq.com>
2020-10-17 11:15:06 +0800

    field

--- obj/fields.py
+++ obj/fields.py
@@ -1,6 +1,33 @@
 import util
 
 
+class Field(object):
+    def __init__(self):
+        self.TINYINT = Tinyint
+        self.SMALLINT = Smallint
+        self.MEDIUMINT = Mediumint
+        self.INT = Int
+        self.BIGINT = Bigint
+        self.BOOL = Bool
+        self.FLOAT = Float
+        self.DOUBLE = Double
+        self.DECIMAL = Decimal
+        self.CHAR = Char
+        self.VARCHAR = Varchar
+        self.DATETIME = Datetime
+        self.DATE = Date
+        self.TIMESTAMP = Timestamp
+        self.TIME = Time
+        self.YEAR = Year
+        self.TINYTEXT = Tinytext
+        self.TEXT = Text
+        self.MEDIUMTEXT = Mediumtext
+        self.LONGTEXT = Longtext
+        self.BLOB = Blob
+        self.ENUM = Enum
+        self.SET = Set
+
+
 class FieldBase(object):
     def __init__(self):
         self.uuid = util.uuid_string(upper=True)
@@ -19,9 +46,6 @@ class IntegerNumber(FieldBase):
         if zerofill and (not self.unsigned):
             self.unsigned = True
 
-    def __str__(self):
-        raise TypeError("Invalid base function called.")
-
 
 class Tinyint(IntegerNumber):
     def __init__(self, length=0):
@@ -29,6 +53,14 @@ class Tinyint(IntegerNumber):
         self.default_length = 4
         if self.length < self.default_length:
             self.length = self.default_length
+
+    def __str__(self):
+        if self.zerofill:
+            return "tinyint(%s) zerofill" % self.length
+        elif self.unsigned:
+            return "tinyint(%s) unsigned" % self.length
+        else:
+            return "tinyint(%s)" % self.length
 
 
 class Smallint(IntegerNumber):
@@ -38,6 +70,14 @@ class Smallint(IntegerNumber):
         if self.length < self.default_length:
             self.length = self.default_length
 
+    def __str__(self):
+        if self.zerofill:
+            return "smallint(%s) zerofill" % self.length
+        elif self.unsigned:
+            return "smallint(%s) unsigned" % self.length
+        else:
+            return "smallint(%s)" % self.length
+
 
 class Mediumint(IntegerNumber):
     def __init__(self, length=0):
@@ -45,6 +85,14 @@ class Mediumint(IntegerNumber):
         self.default_length = 9
         if self.length < self.default_length:
             self.length = self.default_length
+
+    def __str__(self):
+        if self.zerofill:
+            return "mediumint(%s) zerofill" % self.length
+        elif self.unsigned:
+            return "mediumint(%s) unsigned" % self.length
+        else:
+            return "mediumint(%s)" % self.length
 
 
 class Int(IntegerNumber):
@@ -54,6 +102,14 @@ class Int(IntegerNumber):
         if self.length < self.default_length:
             self.length = self.default_length
 
+    def __str__(self):
+        if self.zerofill:
+            return "int(%s) zerofill" % self.length
+        elif self.unsigned:
+            return "int(%s) unsigned" % self.length
+        else:
+            return "int(%s)" % self.length
+
 
 class Bigint(IntegerNumber):
     def __init__(self, length=0):
@@ -62,9 +118,18 @@ class Bigint(IntegerNumber):
         if self.length < self.default_length:
             self.length = self.default_length
 
+    def __str__(self):
+        if self.zerofill:
+            return "bigint(%s) zerofill" % self.length
+        elif self.unsigned:
+            return "bigint(%s) unsigned" % self.length
+        else:
+            return "bigint(%s)" % self.length
+
 
 class Bool(FieldBase):
-    pass
+    def __str__(self):
+        return "bool"
 
 
 class FloatNumber(FieldBase):
@@ -110,24 +175,96 @@ class String(FieldBase):
 
 
 class Char(String):
-    pass
+    def __str__(self):
+        return "char(%s)" % self.length
 
 
 class Varchar(String):
-    pass
+    def __str__(self):
+        return "varchar(%s)" % self.length
 
 
 class Datetime(FieldBase):
-    def __init__(self, date):
+    def __str__(self):
+        return "datetime"
+
+
+class Date(FieldBase):
+    def __str__(self):
+        return "date"
+
+
+class Timestamp(FieldBase):
+    def __str__(self):
+        return "timestamp"
+
+
+class Time(FieldBase):
+    def __str__(self):
+        return "time"
+
+
+class Year(FieldBase):
+    def __str__(self):
+        return "year"
+
+
+class Tinytext(FieldBase):
+    def __str__(self):
+        return "tinytext"
+
+
+class Text(FieldBase):
+    def __str__(self):
+        return "text"
+
+
+class Mediumtext(FieldBase):
+    def __str__(self):
+        return "mediumtext"
+
+
+class Longtext(FieldBase):
+    def __str__(self):
+        return "longtext"
+
+
+class Blob(FieldBase):
+    def __str__(self):
+        return "blob"
+
+
+class Enum(FieldBase):
+    def __init__(self, data_list):
         super().__init__()
-        self.year = date.year
-        self.month = date.month
-        self.day = date.day
-        self.hour = date.hour
-        self.minute = date.minute
-        self.second = date.second
+        if len(data_list) <= 0:
+            raise TypeError("SQL syntax error for enum: Empty data list.")
+        if len(data_list) > 10:
+            data_list = data_list[:10]
+        if len(set(data_list)) != len(data_list):
+            raise TypeError("Duplicate element for enum list.")
+        self.data_list = data_list
 
     def __str__(self):
-        return str(self.year) + "-" + str(self.month) + "-" + str(self.day)\
-            + " " + str(self.hour) + ":" + str(self.minute) + ":" + \
-            str(self.second)
+        data = ",".join(
+            ["\'" + element + "\'" for element in self.data_list]
+        )
+        return "enum(%s)" % data
+
+
+class Set(FieldBase):
+    def __init__(self, data_list):
+        super().__init__()
+        if len(data_list) <= 0:
+            raise TypeError("SQL syntax error for set: Empty data list.")
+        if len(data_list) > 10:
+            data_list = data_list[:10]
+        if len(set(data_list)) != len(data_list):
+            raise TypeError("Duplicate element for set list.")
+        self.data_list = data_list
+
+    def __str__(self):
+        data = ",".join(
+            ["\'" + element + "\'" for element in self.data_list]
+        )
+        return "set(%s)" % data
